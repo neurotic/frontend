ce621818b78f3bb28162d353b1b2c34327936179
Carlos <carlos@edukame.com>
2019-01-03 18:30:10 +0100

    Cambios fotos

--- src/components/Pictures/Picture/Picture.js
+++ src/components/Pictures/Picture/Picture.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import classes from './Picture.css';
 
-const picture = ( props ) => (
-    <div className={classes.Picture}>
-        <img src="" />
-    </div>
-);
+class Picture extends Component {
+    state = {
+        pictures: [
+            { id: 0, title: 'Angular - The Complete Guide', src: 'https://uploads.toptal.io/blog/image/125395/toptal-blog-image-1518187252525-03f6db7b1c131066061024c236c7e3ff.png' },
+            { id: 1, title: 'Vue - The Complete Guide', src: 'https://miro.medium.com/max/980/0*BRrBBgjCGAzRniV8.jpg' },
+            { id: 2, title: 'PWA - The Complete Guide', src: 'https://developers.google.com/web/updates/images/2015/12/getting-started-pwa/pwa-general-0-@1x.jpg' }
+        ]
+    }
 
-export default picture;
+    render () {
+        const id_picture = this.props.match.params.id;
+        return (
+            <div className={classes.Picture}>
+                <h1>{this.state.pictures[id_picture].title}</h1>
+                <img src={this.state.pictures[id_picture].src} />
+            </div>
+        );
+    }
+}
+
+export default Picture;
--- src/components/Pictures/Pictures.js
+++ src/components/Pictures/Pictures.js
@@ -1,14 +1,38 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 import classes from './Pictures.css';
 import Picture from './Picture/Picture.js';
 
-const pictures = ( props ) => (
-    <ul>
-        <li>
-            <Picture />
-        </li>
-    </ul>
-);
 
-export default pictures;
+
+class Pictures extends Component {
+    state = {
+        pictures: [
+            { id: 0, title: 'Angular - The Complete Guide', src: 'https://uploads.toptal.io/blog/image/125395/toptal-blog-image-1518187252525-03f6db7b1c131066061024c236c7e3ff.png' },
+            { id: 1, title: 'Vue - The Complete Guide', src: 'https://miro.medium.com/max/980/0*BRrBBgjCGAzRniV8.jpg' },
+            { id: 2, title: 'PWA - The Complete Guide', src: 'https://developers.google.com/web/updates/images/2015/12/getting-started-pwa/pwa-general-0-@1x.jpg' }
+        ]
+    }
+
+    render () {
+        return (    
+            <div>
+            <h1>Flicker Pictures</h1>
+            <section className="Pictures">
+                {
+                    this.state.pictures.map( pic => {
+                        return (
+                            <Link to={'/pic/' + pic.id} key={pic.id}>
+                                <article className="Pic">{pic.title}</article>
+                            </Link>
+                        );
+                    } )
+                }
+            </section>
+            </div>
+        );
+    }
+}
+
+export default Pictures;
